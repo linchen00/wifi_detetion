9be8a570d3eafa416fd4757d04bd6bd89feb8029
arthur <linchen00@foxmail.com>
2024-04-05 18:36:38 +0800

    feat:暂存

--- ios/Classes/WifiDetectionPlugin.swift
+++ ios/Classes/WifiDetectionPlugin.swift
@@ -15,16 +15,36 @@ public class WifiDetectionPlugin: NSObject, FlutterPlugin {
         switch call.method {
         case "getPlatformVersion":
             result("iOS " + UIDevice.current.systemVersion)
-            Task{
-               let name1 = await MDNSResolver(timeout: TimeInterval(5)).resolve(ip: "192.168.0.123")
-                print("name1:\(name1)")
-            }
-            
-           let name =  NetBIOSResolver().resolve(ip: "192.168.0.253")
+            let name = getHostName(for: "192.168.0.123")
             print("name:\(name)")
-            
+    
         default:
             result(FlutterMethodNotImplemented)
         }
+    }
+    
+    func getHostName(for ipAddress: String) -> String? {
+        var hostname = ""
+
+        // 将 IP 地址转换为 C 字符串
+        let cIpAddress = ipAddress.cString(using: .utf8)
+
+        // 将 C 字符串转换为 in_addr 结构体
+        var addr = in_addr()
+        if let cIpAddress = cIpAddress {
+            inet_aton(cIpAddress, &addr)
+        } else {
+            return nil
+        }
+
+        // 通过 IP 地址获取主机名
+        guard let host = gethostbyaddr(&addr, socklen_t(MemoryLayout<in_addr>.size), AF_INET),
+              let hostnamePtr = host.pointee.h_name else {
+            return nil
+        }
+
+        // 从返回的 host 结构体中获取主机名
+
+        return String(cString: hostnamePtr)
     }
 }
